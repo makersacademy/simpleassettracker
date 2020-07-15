44beec9c728ff40128a5918a383907e9ac6bbf2f
Richard Pattinson <richie.pattinson@googlemail.com>
2020-07-15 11:02:55 +0100

    added failing test

--- AssetTracker/test.py
+++ AssetTracker/test.py
@@ -4,3 +4,5 @@ class FirstTest(TestCase):
     def test_something_that_will_pass(self):
         self.assertFalse(False)
 
+    def test_something_that_will_fail(self):
+        self.assertTrue(False)
