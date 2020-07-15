d692277ea06822812dbee5b7c278ae3ede188d90
Dawidek <dawidszpener141@gmail.com>
2020-07-15 10:12:07 +0100

    chore: first test

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from models import test_method
 
-# Create your tests here.
+# models test
+class ModelsTest(TestCase):
+  def test(self):
+    num = 10
+    outcome = test_method(num)
+    self.assertEqual(outcome, 15)
+
