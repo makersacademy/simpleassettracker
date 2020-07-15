b4a836c2460656f47ce0fcd1831f3b1995700a2f
Richard Pattinson <richie.pattinson@googlemail.com>
2020-07-15 11:00:18 +0100

    removed failing test

--- AssetTracker/test.py
+++ AssetTracker/test.py
@@ -4,5 +4,3 @@ class FirstTest(TestCase):
     def test_something_that_will_pass(self):
         self.assertFalse(False)
 
-    def test_something_that_will_fail(self):
-        self.assertTrue(False)
