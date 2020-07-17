437b80b333b48ba1ed0d28af2ef668d604cfa231
Dawidek <dawidszpener141@gmail.com>
2020-07-17 12:22:15 +0100

    chore: heroku g var

--- AssetTracker/settings.py
+++ AssetTracker/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG', default=False)
+DEBUG = os.environ.get('DEBUG', default=False)
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
@@ -124,3 +124,4 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 django_heroku.settings(locals())
+del DATABASES['default']['OPTIONS']['sslmode']
