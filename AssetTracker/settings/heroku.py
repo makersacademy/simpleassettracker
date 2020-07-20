b6df9924a560df728b30653676ce0e33b31fd3ff
Dawidek <dawidszpener141@gmail.com>
2020-07-20 12:08:30 +0100

    chore: heroku fix

--- AssetTracker/settings/heroku.py
+++ AssetTracker/settings/heroku.py
@@ -5,7 +5,7 @@ Production Settings for Heroku
 import environ
 
 # If using in your own project, update the project namespace below
-from gettingstarted.settings.base import *
+from AssetTracker.settings.base import *
 
 env = environ.Env(
     # set casting, default value
