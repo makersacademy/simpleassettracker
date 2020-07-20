383fa9af58e4e53c0e7ff66576e8c632591332c1
Dawidek <dawidszpener141@gmail.com>
2020-07-20 10:25:06 +0100

    chore: heroku fix

--- AssetTracker/settings.py
+++ AssetTracker/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
