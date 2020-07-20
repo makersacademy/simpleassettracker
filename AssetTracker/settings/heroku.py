c2ac406dd15b056042d2f754673bfd1555c58135
Dawidek <dawidszpener141@gmail.com>
2020-07-20 11:04:17 +0100

    chore: heroku fix

---
+++ AssetTracker/settings/heroku.py
@@ -0,0 +1,25 @@
+"""
+Production Settings for Heroku
+"""
+
+import environ
+
+# If using in your own project, update the project namespace below
+from gettingstarted.settings.base import *
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+# False if not in os.environ
+DEBUG = env('DEBUG')
+
+# Raises django's ImproperlyConfigured exception if SECRET_KEY not in os.environ
+SECRET_KEY = env('SECRET_KEY')
+
+# Parse database connection url strings like psql://user:pass@127.0.0.1:8458/db
+DATABASES = {
+    # read os.environ['DATABASE_URL'] and raises ImproperlyConfigured exception if not found
+    'default': env.db(),
+}
