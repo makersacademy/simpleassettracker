7851e8ef7745e7e7c021146a076c24ae8d6e3a9c
Dawidek <dawidszpener141@gmail.com>
2020-07-24 10:33:56 +0100

    rendering a dashboard template

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.http import HttpResponse
 
 
-def homePageView(request):
-  return HttpResponse("Hello World!")
+def homePageView(response):
+  return render(response, "dashboard/dashboard.html", {})
