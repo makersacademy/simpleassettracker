f21e9f81e27dd8d6e44d3aafafba28af7b834b54
Dawidek <dawidszpener141@gmail.com>
2020-07-30 12:01:44 +0100

    test database fix

--- AssetTracker/settings/base.py
+++ AssetTracker/settings/base.py
@@ -139,9 +139,3 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
 del DATABASES['default']['OPTIONS']['sslmode']
-
-import sys
-if 'test' in sys.argv:
-    DATABASES = {
-    'default': env.db(),
-    }
