45789b7e091d8011888c0f8818e257c30a812880
Dawidek <dawidszpener141@gmail.com>
2020-07-30 12:28:15 +0100

    test database fix

--- AssetTracker/settings/base.py
+++ AssetTracker/settings/base.py
@@ -139,15 +139,3 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
 del DATABASES['default']['OPTIONS']['sslmode']
-
-# import sys
-# if 'test' in sys.argv:
-#     DATABASES = {
-#         'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'test',
-#         'USER': os.environ.get('PGUSER'),
-#         'PASSWORD': os.environ.get('PGPASSWORD'),
-#         'HOST': '127.0.0.1',
-#     }
-# }
