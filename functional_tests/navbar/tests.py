933ef1a9e1d06bafee9ad9f4915018ecedb02e00
Dawidek <dawidszpener141@gmail.com>
2020-07-31 09:44:41 +0100

    test database fix

--- functional_tests/navbar/tests.py
+++ functional_tests/navbar/tests.py
@@ -12,6 +12,7 @@ class NavBar(LiveServerTestCase):
 
   def setUp(self):
     self.browser = webdriver.Firefox()
+    self.browser.implicitly_wait(3)
 
   def tearDown(self):
     self.browser.quit()
