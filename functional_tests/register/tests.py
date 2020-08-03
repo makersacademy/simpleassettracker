5838fd44254ba989e6fded4cef8a2a259e11c6d4
Catriona <catrionamairi@blueyonder.co.uk>
2020-08-03 11:22:00 +0100

    Add existing user to login with

--- functional_tests/register/tests.py
+++ functional_tests/register/tests.py
@@ -4,12 +4,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from django.test import LiveServerTestCase
+from django.contrib.auth.models import User
 import time
 
 class AdminTest(LiveServerTestCase):
-  fixtures = ['functional_tests/fixtures/admin.json']
 
   def setUp(self):
+    self.user = User.objects.create_user(username='admin1', password='admin1', email='test@test.com', is_active=True)
+    self.user.save()
     self.browser = webdriver.Firefox()
 
   def tearDown(self):
@@ -20,9 +22,9 @@ class AdminTest(LiveServerTestCase):
     body = self.browser.find_element_by_tag_name('body')
     self.assertIn('Django administration', body.text)
     username_field = self.browser.find_element_by_name('username')
-    username_field.send_keys('admin')
+    username_field.send_keys('admin1')
     password_field = self.browser.find_element_by_name('password')
-    password_field.send_keys('admin')
+    password_field.send_keys('admin1')
     password_field.send_keys(Keys.RETURN)
     wait = WebDriverWait(self.browser, 5)
     wait.until(EC.text_to_be_present_in_element((By.ID, "content"), 'Site administration'))
@@ -76,6 +78,8 @@ class LoginAndOutTest(LiveServerTestCase):
 
   def setUp(self):
     self.browser = webdriver.Firefox()
+    self.user = User.objects.create_user(username='admin1', password='admin1', email='test@test.com', is_active=True)
+    self.user.save()
 
   def tearDown(self):
     self.browser.quit()
@@ -84,19 +88,20 @@ class LoginAndOutTest(LiveServerTestCase):
     # loggin in
     self.browser.get(self.live_server_url + '/login')
     username_field = self.browser.find_element_by_id('id_username')
-    username_field.send_keys('admin')
+    username_field.send_keys('admin1')
     password_field = self.browser.find_element_by_id('id_password')
-    password_field.send_keys('admin')
+    password_field.send_keys('admin1')
     password_field.send_keys(Keys.RETURN)
-    time.sleep(1)
+    wait = WebDriverWait(self.browser, 5)
+    wait.until(EC.text_to_be_present_in_element((By.ID, "content"), 'Hello world'))
     body = self.browser.find_element_by_tag_name('body')
 
-    self.assertIn('admin', body.text)
+    self.assertIn('admin1', body.text)
     logout = self.browser.find_element_by_id('id_logout')
     logout.send_keys(Keys.RETURN)
-    time.sleep(1)
+    wait.until(EC.text_to_be_present_in_element((By.ID, "content"), 'Hello world'))
     body = self.browser.find_element_by_tag_name('body')
-    self.assertNotIn('admin', body.text)
+    self.assertNotIn('admin1', body.text)
 
 # class IndexTest(LiveServerTestCase):
 #   def test_hello_world(self):
