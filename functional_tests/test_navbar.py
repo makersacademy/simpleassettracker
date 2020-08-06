2e67c2dadad11b89b2e560d2bd3859822f3f1584
Haydon Houghton <haydonhoughton@outlook.com>
2020-08-06 11:50:46 +0100

    Add test for navbar asset button

--- functional_tests/test_navbar.py
+++ functional_tests/test_navbar.py
@@ -4,19 +4,32 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from django.test import LiveServerTestCase
+from django.contrib.auth.models import User
 import time
 
 class NavBar(LiveServerTestCase):
 
   def setUp(self):
     self.browser = webdriver.Firefox()
+    self.user = User.objects.create_user(username='admin1', password='admin1', email='test@test.com', is_active=True)
+    self.user.save()
 
   def tearDown(self):
     self.browser.quit()
+  
+  def login(self):
+    self.browser.get(self.live_server_url + '/login')
+    username_field = self.browser.find_element_by_id('id_username')
+    username_field.send_keys('admin1')
+    password_field = self.browser.find_element_by_id('id_password')
+    password_field.send_keys('admin1')
+    password_field.send_keys(Keys.RETURN)
+    wait = WebDriverWait(self.browser, 5)
+    wait.until(EC.text_to_be_present_in_element((By.ID, "content"), 'Hello'))
 
   def test_home_button(self):
     self.browser.get(self.live_server_url + '/login')
-    home = self.browser.find_element_by_id('homeButton')
+    home = self.browser.find_element_by_id('id_navbar_home')
     home.send_keys(Keys.RETURN)
     wait = WebDriverWait(self.browser, 5)
     wait.until(EC.text_to_be_present_in_element((By.ID, "content"), 'Hello world'))
@@ -25,7 +38,7 @@ class NavBar(LiveServerTestCase):
 
   def test_register_button(self):
     self.browser.get(self.live_server_url + '/login')
-    register = self.browser.find_element_by_id('registerButton')
+    register = self.browser.find_element_by_id('id_navbar_register')
     register.send_keys(Keys.RETURN)
     wait = WebDriverWait(self.browser, 5)
     wait.until(EC.text_to_be_present_in_element((By.ID, "content"), 'Register here'))
@@ -34,9 +47,18 @@ class NavBar(LiveServerTestCase):
 
   def test_login_button(self):
     self.browser.get(self.live_server_url)
-    register = self.browser.find_element_by_id('id_login')
+    register = self.browser.find_element_by_id('id_navbar_login')
     register.send_keys(Keys.RETURN)
     wait = WebDriverWait(self.browser, 5)
     wait.until(EC.text_to_be_present_in_element((By.ID, "content"), 'Login Here'))
     body = self.browser.find_element_by_tag_name('body')
     self.assertIn("Don't have an account?", body.text)
+
+  def test_asset_button(self):
+    with self.settings(DEBUG=True):
+      self.login()
+      asset = self.browser.find_element_by_id('id_navbar_asset')
+      asset.send_keys(Keys.RETURN)
+      time.sleep(1)
+      body = self.browser.find_element_by_tag_name('body')
+      self.assertIn("Assets", body.text)
