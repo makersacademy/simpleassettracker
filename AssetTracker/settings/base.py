5521948db301838b4b3f2d7fca8169f0d6ecbad0
Catriona <catrionamairi@blueyonder.co.uk>
2020-08-06 13:05:27 +0100

    Add password hashing to seeds

--- AssetTracker/settings/base.py
+++ AssetTracker/settings/base.py
@@ -117,6 +117,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
