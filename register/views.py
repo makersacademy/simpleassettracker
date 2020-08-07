3c881cd274bec35f8dd7aa1fe25c91c179ff9483
Haydon Houghton <haydonhoughton@outlook.com>
2020-08-07 12:19:41 +0100

    Add success and error messages on registration

--- register/views.py
+++ register/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from django.contrib import messages
 
 # Create your views here.
 def register(response):
@@ -7,12 +8,14 @@ def register(response):
 		form = RegisterForm(response.POST)
 		if form.is_valid():
 			form.save()
+			messages.success(response, 'Your account has been created! Please sign in.')
 			return redirect("/")
 
 		else:
 			form = RegisterForm()
+			messages.error(response, 'Invalid form submission')
 			return render(response, "register/register.html", {"form": form})
-			
+
 	else:
 		form = RegisterForm()
 		return render(response, "register/register.html", {"form": form})
