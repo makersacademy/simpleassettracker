daa29c1ed18bd140b13765f7870a6111074693f0
Haydon Houghton <haydonhoughton@outlook.com>
2020-08-07 11:47:53 +0100

    Reroute back to register page on failed register attempt

--- register/views.py
+++ register/views.py
@@ -3,13 +3,16 @@ from .forms import RegisterForm
 
 # Create your views here.
 def register(response):
-    if response.method == "POST":
-	    form = RegisterForm(response.POST)
-	    if form.is_valid():
-	        form.save()
+	if response.method == "POST":
+		form = RegisterForm(response.POST)
+		if form.is_valid():
+			form.save()
+			return redirect("/")
 
-	    return redirect("/")
-    else:
-	    form = RegisterForm()
-
-    return render(response, "register/register.html", {"form":form})
+		else:
+			form = RegisterForm()
+			return render(response, "register/register.html", {"form": form})
+			
+	else:
+		form = RegisterForm()
+		return render(response, "register/register.html", {"form": form})
