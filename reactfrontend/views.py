1c930184f2017c22b44d8a674004923ff46dcb15
richardpattinson <44261976+richardpattinson@users.noreply.github.com>
2020-08-10 10:45:30 +0100

    add authentication requirement to asset view (#59)

--- reactfrontend/views.py
+++ reactfrontend/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+@login_required(login_url='/login')
 def assets(request):
     return render(request, 'reactfrontend/assets.html')
