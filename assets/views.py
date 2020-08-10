9b3e9009f8e17b5b70e9902f5f2ab6268afd4605
Dawid Szpener <54997333+DawidSzpener@users.noreply.github.com>
2020-08-10 11:49:56 +0100

    Reactdeleteasset (#62)
    * delete request 403in
    
    * user can now delete assets, added csrftoken retrieval method
    
    * add assetform.js
    
    * fixed Asset -> Your Asset for test
    
    * added static css files to gitignore
    
    * added django staticfiles to gitignore
    
    * ignoring staticfiles
    
    * updating staticfiles to pass tests
    
    Co-authored-by: Richard Pattinson <richie.pattinson@googlemail.com>
    Co-authored-by: richardpattinson <44261976+richardpattinson@users.noreply.github.com>

--- assets/views.py
+++ assets/views.py
@@ -5,3 +5,9 @@ from rest_framework import generics
 class AssetListCreate(generics.ListCreateAPIView):
     queryset = Asset.objects.all()
     serializer_class = AssetSerializer
+
+class AssetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Asset.objects.all()
+    serializer_class = AssetSerializer
+
+
--- reactfrontend/src/components/App.js
+++ reactfrontend/src/components/App.js
@@ -3,35 +3,61 @@ import { render } from "react-dom";
 
 class App extends Component {
     constructor(props) {
-      super(props);
-      this.state = {
-        data: [],
-        loaded: false,
-        placeholder: "Loading"
-      };
+        super(props);
+        this.state = {
+            data: [],
+            loaded: false,
+            placeholder: "Loading"
+        };
     }
   
     componentDidMount() {
-      fetch("api/asset")
-        .then(response => {
-          if (response.status > 400) {
-            return this.setState(() => {
-              return { placeholder: "Something went wrong!" };
+        fetch("api/asset")
+            .then(response => {
+            if (response.status > 400) {
+                return this.setState(() => {
+                return { placeholder: "Something went wrong!" };
+                });
+            }
+            return response.json();
+            })
+            .then(data => {
+                console.log(data)
+                this.setState(() => {
+                    return {
+                    data,
+                    loaded: true
+                    };
+                });
             });
-          }
-          return response.json();
-        })
-        .then(data => {
-          console.log(data)
-          this.setState(() => {
-            return {
-              data,
-              loaded: true
-            };
-          });
-        });
     }
-  
+
+    getCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1,c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        }
+        return null;
+    }
+
+
+    handleDelete(asset_object) {
+    fetch(`/assets/api/asset/${asset_object.id}`, {
+        method: 'DELETE',
+        headers: {
+            "X-CSRFToken": this.getCookie('csrftoken'),
+            'Content-Type': 'application/json',
+        },
+    })
+    .then(() => {
+        this.setState({data: this.state.data.filter(asset => asset_object.id !== asset.id)})
+    });
+    };
+
+
     render() {
       return (
           <div>
@@ -40,7 +66,9 @@ class App extends Component {
             {this.state.data.map(asset => {
                 return (
                 <li key={asset.id}>
+                    <button onClick={() => this.handleDelete(asset)}>Delete</button>
                     {asset.AssetTag} - {asset.DeviceType} - {asset.CreatedBy}
+
                 </li>
                 );
             })}
