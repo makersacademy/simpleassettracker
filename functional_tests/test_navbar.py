83f65096d3e356ee73c440010a74e80c0b8e2edb
Dawidek <dawidszpener141@gmail.com>
2020-08-11 11:00:42 +0100

    added tests for navbar add assets button and redirection

--- functional_tests/test_navbar.py
+++ functional_tests/test_navbar.py
@@ -62,11 +62,25 @@ class NavBar(LiveServerTestCase):
       time.sleep(1)
       body = self.browser.find_element_by_tag_name('body')
       self.assertIn("Your Assets", body.text)
+
+  def test_add_asset_button(self):
+    with self.settings(DEBUG=True):
+      self.login()
+      add_asset = self.browser.find_element_by_id('id_navbar_asset_add')
+      add_asset.send_keys(Keys.RETURN)
+      time.sleep(1)
+      body = self.browser.find_element_by_tag_name('body')
+      self.assertIn("Add an Asset", body.text)
   
   def test_asset_button_not_viewable_when_logged_out(self):
     self.browser.get(self.live_server_url)
     body = self.browser.find_element_by_tag_name('body')
     self.assertNotIn('Assets', body.text)
+  
+  def test_add_asset_button_not_viewable_when_logged_out(self):
+    self.browser.get(self.live_server_url)
+    body = self.browser.find_element_by_tag_name('body')
+    self.assertNotIn('Add Asset', body.text)
 
   def test_logout_button_not_viewable_when_logged_out(self):
     self.browser.get(self.live_server_url)
