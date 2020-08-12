d51bcadfe6e67df20d96b56a831fc409c22a8e20
Haydon Houghton <haydonhoughton@outlook.com>
2020-08-12 11:38:10 +0100

    Add User ID to window for react component

--- reactfrontend/src/components/addAssetForm/addAssetForm.js
+++ reactfrontend/src/components/addAssetForm/addAssetForm.js
@@ -14,11 +14,15 @@ class AddAssetForm extends Component {
       },
     }
     this.submitHandler = this.submitHandler.bind(this)
+    this.changeHandler = this.changeHandler.bind(this)
   }
 
-  getUserToken() {
-    const token = localStorage.getItem("token")
-    console.log(token)
+  componentDidMount() {
+    const newData = {...this.state.asset}
+    let newDataUser = {...newData["createdBy"]}
+    newDataUser = window.django.user.user_id
+    newData["createdBy"] = newDataUser
+    this.setState({asset: newData})
   }
 
   submitHandler(event) {
@@ -26,22 +30,25 @@ class AddAssetForm extends Component {
     console.log('this ran')
   }
 
-  changeHandler(event, indentifier) {
+  changeHandler(event, identifier) {
     event.preventDefault()
-    const oldData = {...this.state.asset}
-    console.log(indentifier)
-    console.log(oldData)
+    const newData = {...this.state.asset}
+    let newDataElement = {...newData[identifier]}
+    newDataElement = event.target.value
+    newData[identifier] = newDataElement
+    console.log(identifier)
+    console.log(newData)
+    this.setState({asset: newData})
   }
   
   render() {
 
     return(
       <div>
-        {this.getUserToken()}
         <h1>Add an Asset</h1>
         <form onSubmit={this.submitHandler}>
-          <input inputtype='input' type="text" onChange={(event) => this.changeHandler(event, 'assetTag')} name="AssetTag" id="id_add_asset_tag" placeholder="Asset Tag"></input>
-          <input inputtype='select' type="text" onChange={(event) => this.changeHandler(event, 'assetType')} name="AssetType" id="id_add_asset_type" placeholder="Select asset type"></input>
+          <input inputtype='input' type="text" onChange={(event) => this.changeHandler(event, 'assetTag')} name="assetTag" id="id_add_asset_tag" placeholder="Asset Tag"></input>
+          <input inputtype='select' type="text" onChange={(event) => this.changeHandler(event, 'assetType')} name="assetType" id="id_add_asset_type" placeholder="Select asset type"></input>
           <button className='submitButton' id="id_add_asset_submit" type="submit" value="submit">Add Asset</button>
         </form>
       </div>
