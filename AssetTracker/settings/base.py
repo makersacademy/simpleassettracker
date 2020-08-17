26a23ff1ed3dbd86db30bd09b1f449b0f41eb93e
Haydon Houghton <haydonhoughton@outlook.com>
2020-08-17 11:50:36 +0100

    Configure static files for styling

--- AssetTracker/settings/base.py
+++ AssetTracker/settings/base.py
@@ -145,6 +145,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, '../static')
+]
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
