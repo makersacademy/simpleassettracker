abaac624601ef55240e068548196f896fde632eb
Catriona <catrionamairi@blueyonder.co.uk>
2020-08-19 15:06:49 +0100

    Add mobile count to dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,9 +4,11 @@ from assets.models import Asset
 def dashboardPageView(response):
   asset_count = countAssets()
   laptop_count = countLaptops()
+  mobile_count = countMobiles()
   return render(response, "dashboard/dashboard.html", {
   "asset_count": asset_count,
-  "laptop_count": laptop_count
+  "laptop_count": laptop_count,
+  "mobile_count": mobile_count
   })
 
 def countAssets():
@@ -14,3 +16,6 @@ def countAssets():
 
 def countLaptops():
   return Asset.objects.filter(DeviceType="laptop").count()
+
+def countMobiles():
+  return Asset.objects.filter(DeviceType="mobile").count()
