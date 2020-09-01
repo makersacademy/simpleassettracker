8da53b8d28b042c45fd2c5d1cbf30841be47eb7f
Catriona <catrionamairi@blueyonder.co.uk>
2020-09-01 18:45:58 +0100

    Add test for non matching passwords

--- functional_tests/test_register.py
+++ functional_tests/test_register.py
@@ -51,7 +51,7 @@ class SignUpTest(LiveServerTestCase):
       message = self.browser.find_element_by_class_name('messages')
       self.assertIn('Your account has been created! Please sign in', message.text)
 
-  def test_invalid_user_signup(self):
+  def test_too_obvious_password(self):
     with self.settings(DEBUG=True):
       self.browser.get(self.live_server_url + '/register')
       username_field = self.browser.find_element_by_id('id_username')
@@ -60,6 +60,22 @@ class SignUpTest(LiveServerTestCase):
       email_field.send_keys('adam@example.com')
       password_field = self.browser.find_element_by_id('id_password1')
       password_field.send_keys('password')
+      password_confirmation_field = self.browser.find_element_by_id('id_password2')
+      password_confirmation_field.send_keys('password')
+      password_field.send_keys(Keys.RETURN)
+      time.sleep(1)
+      message = self.browser.find_element_by_class_name('messages')
+      self.assertIn('Invalid form submission', message.text)
+
+  def test_passwords_not_the_same(self):
+    with self.settings(DEBUG=True):
+      self.browser.get(self.live_server_url + '/register')
+      username_field = self.browser.find_element_by_id('id_username')
+      username_field.send_keys('adam')
+      email_field = self.browser.find_element_by_id('id_email')
+      email_field.send_keys('adam@example.com')
+      password_field = self.browser.find_element_by_id('id_password1')
+      password_field.send_keys('Th1sIsAT3st.')
       password_confirmation_field = self.browser.find_element_by_id('id_password2')
       password_confirmation_field.send_keys('password')
       password_field.send_keys(Keys.RETURN)
