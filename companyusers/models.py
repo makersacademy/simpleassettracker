917bb8c218cf07770aed455e34375d6baecb9c7e
richardpattinson <richie.pattinson@googlemail.com>
2020-09-01 13:02:48 +0100

    companyuser table autopopulates when user is created

--- companyusers/models.py
+++ companyusers/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from companies.models import Company
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class CompanyUser(models.Model):
   User = models.OneToOneField(
@@ -9,3 +11,13 @@ class CompanyUser(models.Model):
         primary_key=True,
     )
   Company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+@receiver(post_save, sender=User)
+def create_companyuser(sender, instance, created, **kwargs):
+    if created:
+        C = CompanyUser.objects.create(User=instance, Company=Company.objects.get(id=1))
+        C.save()
+
+# @receiver(post_save, sender=User)
+# def save_companyuser(sender, instance, **kwargs):
+#     instance.CompanyUser.save()
