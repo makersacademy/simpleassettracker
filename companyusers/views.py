af69740193457bf987541de4a7da74d9eb0c53ab
Haydon Houghton <haydonhoughton@outlook.com>
2020-09-04 12:34:46 +0100

    Create API view to get company users of current company

--- companyusers/views.py
+++ companyusers/views.py
@@ -3,9 +3,20 @@ from .serializers import CompanyUserSerializer
 from rest_framework import generics
 
 class CompanyUserListCreate(generics.ListCreateAPIView):
-    queryset = CompanyUser.objects.all()
-    serializer_class = CompanyUserSerializer
+  queryset = CompanyUser.objects.all()
+  serializer_class = CompanyUserSerializer
 
 class CompanyUserDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = CompanyUser.objects.all()
-    serializer_class = CompanyUserSerializer
+  queryset = CompanyUser.objects.all()
+  serializer_class = CompanyUserSerializer
+
+class CompanyUserList(generics.ListCreateAPIView):
+  serializer_class = CompanyUserSerializer
+
+  def get_queryset(self):
+    queryset =  CompanyUser.objects.all()
+    user = self.request.user
+    company_id = CompanyUser.objects.get(User=user).Company.id
+    if company_id is not None:
+      queryset = queryset.filter(Company=company_id)
+    return queryset
