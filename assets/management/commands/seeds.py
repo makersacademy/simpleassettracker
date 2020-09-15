2ff6cfd87edf7e9eeb839e150c411665f0883e72
Haydon Houghton <haydonhoughton@outlook.com>
2020-09-15 10:17:51 +0100

    Add company name option and assign ownership to first seeded user

--- assets/management/commands/seeds.py
+++ assets/management/commands/seeds.py
@@ -16,12 +16,14 @@ class Command(BaseCommand):
 
   def add_arguments(self, parser):
     parser.add_argument('--mode', type=str, help="Mode")
-    parser.add_argument('--number_of_users', type=int, help="Number of users")
-    parser.add_argument('--assets_per_user', type=int, help="Assets per user")
+    parser.add_argument('--number_of_users', type=int, help="Number of users", default=1)
+    parser.add_argument('--assets_per_user', type=int, help="Assets per user", default=2)
+    parser.add_argument('--company_name', type=str, help="Company to create users under", default='SAT')
+
 
   def handle(self, *args, **options):
     self.stdout.write("Seeding data...")
-    run_seed(self, options['mode'], options['number_of_users'], options['assets_per_user'])
+    run_seed(self, options['mode'], options['number_of_users'], options['assets_per_user'], options['company_name'])
     self.stdout.write("Done.")
 
 def clear_data():
@@ -51,10 +53,13 @@ def create_companyuser(user_instance, company_instance):
   CU = CompanyUser(User_id=user_instance.id, Company_id=company_instance.id)
   CU.save()
 
-def create_objects(users, assets):
-  company = create_company("Makers")
+def create_objects(users, assets, company_name):
+  company = create_company(company_name)
   for i in range(users):
     user = create_user()
+    if i == 0:
+      company.Owned_by=user
+      company.save()
     create_companyuser(user, company)
     for j in range(assets):
       create_asset(user)
@@ -76,11 +81,11 @@ def get_username():
   usernames.remove(username)
   return username
 
-def run_seed(self, mode, number_of_users, assets_per_user):
+def run_seed(self, mode, number_of_users, assets_per_user, company_name):
   clear_data()
   if mode == MODE_CLEAR:
     return
-  create_objects(number_of_users, assets_per_user)
+  create_objects(number_of_users, assets_per_user, company_name)
 
 
 # Asset Types: ["Laptop", "Mobile", "Misc"]
