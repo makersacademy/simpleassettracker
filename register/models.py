f368718cbd11412ab091c418f174d1ef53c26a2c
Haydon Houghton <haydonhoughton@outlook.com>
2020-09-18 12:39:58 +0100

    create Unauthorized user model

--- register/models.py
+++ register/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from companies.models import Company
+from django.contrib.auth.hashers import make_password
 
-# Create your models here.
+class UnauthorizedUser(models.Model):
+  Username = models.CharField(max_length=30)
+  Password = models.CharField(max_length=30)
+  Email = models.CharField(max_length=60)
+  Company = models.ForeignKey(Company, on_delete=models.CASCADE)
