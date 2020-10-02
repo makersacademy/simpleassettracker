925d2e386c23c48a024162a0cf48dd1ea60e8ba3
richardpattinson <richie.pattinson@googlemail.com>
2020-10-02 12:00:51 +0100

    update importcsv to take new fields

--- importcsv/forms.py
+++ importcsv/forms.py
@@ -4,4 +4,4 @@ from assets.models import Asset
 class AssetForm(forms.ModelForm):
   class Meta:
     model = Asset
-    fields = ("AssetTag", "DeviceType", "AssetStatus", "SerialNumber", "CreatedBy", "Company")
+    fields = ("AssetTag", "DeviceType", "DeviceModel", "AssetStatus", "SerialNumber", "AssetCondition", "ScreenSize", "HardDrive", "Ram", "Year", "CreatedBy", "Company")
