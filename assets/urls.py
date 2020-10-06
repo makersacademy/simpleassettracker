3cbbe147d364d0229a19c4e4dfef0f69b3c10a81
IliasMariosG <57366310+IliasMariosG@users.noreply.github.com>
2020-10-06 21:10:59 +0300

    Use 1 tab as indendation

--- assets/urls.py
+++ assets/urls.py
@@ -3,6 +3,6 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('assets/api/asset/', views.AssetListCreate.as_view() ),
-    path('assets/api/asset/<int:pk>/', views.AssetDetail.as_view()),
+	path('assets/api/asset/', views.AssetListCreate.as_view()),
+	path('assets/api/asset/<int:pk>/', views.AssetDetail.as_view()),
 ]
