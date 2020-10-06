79a6b671f7e22e52215eb51d5f2a8afb983557f9
IliasMariosG <57366310+IliasMariosG@users.noreply.github.com>
2020-10-06 21:07:59 +0300

    Work through linter suggestions
    It adds tabs as indendations and fixes bad continuation warning

--- assets/serializers.py
+++ assets/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import Asset
 
 class AssetSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Asset
-        fields = ('id', 'AssetTag', 'DeviceType', 'DeviceModel', 'AssetStatus', 'CreatedBy', 'SerialNumber', 'Company', 'AssetCondition', 'ScreenSize', 'HardDrive', 'Ram', 'Year')
+	class Meta:
+		model = Asset
+		fields = ('id', 'AssetTag', 'DeviceType', 'DeviceModel', 'AssetStatus', 'CreatedBy',
+		          'SerialNumber', 'Company', 'AssetCondition', 'ScreenSize', 'HardDrive', 'Ram', 'Year')
+		
