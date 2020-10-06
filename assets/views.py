b5a09968325d8f37a409dce2285e6567b6f138b4
IliasMariosG <57366310+IliasMariosG@users.noreply.github.com>
2020-10-06 21:14:45 +0300

    Work through linter suggestions
    It uses 1 tab as indendation, removes missing class docstring and missing module docstring

--- assets/views.py
+++ assets/views.py
@@ -1,11 +1,12 @@
+# Third party imports should be placed first
+from rest_framework import generics
 from .models import Asset
 from .serializers import AssetSerializer
-from rest_framework import generics
 
 class AssetListCreate(generics.ListCreateAPIView):
-    queryset = Asset.objects.all()
-    serializer_class = AssetSerializer
+	queryset = Asset.objects.all()
+	serializer_class = AssetSerializer
 
 class AssetDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Asset.objects.all()
-    serializer_class = AssetSerializer
+	queryset = Asset.objects.all()
+	serializer_class = AssetSerializer
