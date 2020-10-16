e53f0a06586d56e58c0cddb4cdf9ef80a468938d
IliasMariosG <57366310+IliasMariosG@users.noreply.github.com>
2020-10-16 13:08:31 +0300

    Fix indentation (W0311)
    It sets the indentation to one tab (of tab size 2)

--- AssetTracker/apps/assets/management/commands/seeds.py
+++ AssetTracker/apps/assets/management/commands/seeds.py
@@ -12,80 +12,80 @@ MODE_REFRESH = 'refresh'
 MODE_CLEAR = 'clear'
 
 class Command(BaseCommand):
-  help = "Seeds the database with usable data"
+	help = "Seeds the database with usable data"
 
-  def add_arguments(self, parser):
-    parser.add_argument('--mode', type=str, help="Mode")
-    parser.add_argument('--number_of_users', type=int, help="Number of users", default=1)
-    parser.add_argument('--assets_per_user', type=int, help="Assets per user", default=2)
-    parser.add_argument('--company_name', type=str, help="Company to create users under", default='SAT')
+	def add_arguments(self, parser):
+		parser.add_argument('--mode', type=str, help="Mode")
+		parser.add_argument('--number_of_users', type=int, help="Number of users", default=1)
+		parser.add_argument('--assets_per_user', type=int, help="Assets per user", default=2)
+		parser.add_argument('--company_name', type=str, help="Company to create users under", default='SAT')
 
 
-  def handle(self, *args, **options):
-    self.stdout.write("Seeding data...")
-    run_seed(self, options['mode'], options['number_of_users'], options['assets_per_user'], options['company_name'])
-    self.stdout.write("Done.")
+	def handle(self, *args, **options):
+		self.stdout.write("Seeding data...")
+		run_seed(self, options['mode'], options['number_of_users'], options['assets_per_user'], options['company_name'])
+		self.stdout.write("Done.")
 
 def clear_data():
-  User.objects.all().delete()
-  Asset.objects.all().delete()
-  Company.objects.all().delete()
-  CompanyUser.objects.all().delete()
+	User.objects.all().delete()
+	Asset.objects.all().delete()
+	Company.objects.all().delete()
+	CompanyUser.objects.all().delete()
 
 def create_user():
-  username = get_username()
-  password = "password1234"
-  user = User.objects.create(username=username)
-  user.set_password(password)
-  user.save()
-  return user
+	username = get_username()
+	password = "password1234"
+	user = User.objects.create(username=username)
+	user.set_password(password)
+	user.save()
+	return user
 
 def create_asset(user_instance, company_instance):
-  A = Asset(asset_tag=get_random_tag(), device_type=get_random_type(), created_by=user_instance, company=company_instance, serial_number=get_random_tag())
-  A.save()
+	A = Asset(asset_tag=get_random_tag(), device_type=get_random_type(), created_by=user_instance, company=company_instance, serial_number=get_random_tag())
+	A.save()
 
 def create_company(name):
-  C = Company(name=name)
-  C.save()
-  return C
+	C = Company(name=name)
+	C.save()
+	return C
 
 def create_companyuser(user_instance, company_instance):
-  CU = CompanyUser(user_id=user_instance.id, company_id=company_instance.id)
-  CU.save()
+	CU = CompanyUser(user_id=user_instance.id, company_id=company_instance.id)
+	CU.save()
 
 def create_objects(users, assets, company_name):
-  company = create_company(company_name)
-  for i in range(users):
-    user = create_user()
-    if i == 0:
-      company.Owned_by=user
-      company.save()
-    create_companyuser(user, company)
-    for j in range(assets):
-      create_asset(user, company)
+	company = create_company(company_name)
+	for i in range(users):
+		user = create_user()
+		if i == 0:
+			company.Owned_by=user
+			company.save()
+		create_companyuser(user, company)
+		for j in range(assets):
+			create_asset(user, company)
 
 def get_random_tag():
-  key=''
-  for i in range(6):
-    key += random.choice(string.ascii_letters + string.digits)
-  return key
+	key=''
+	for i in range(6):
+		key += random.choice(string.ascii_letters + string.digits)
+	return key
 
 def get_random_type():
-  assetTypes = ["Laptop", "Mobile", "Misc"]
-  assetType = random.choice(assetTypes)
-  return assetType
+	assetTypes = ["Laptop", "Mobile", "Misc"]
+	assetType = random.choice(assetTypes)
+	return assetType
 
 usernames = ["admin", "james", "sarah", "richard", "ethan"]
 def get_username():
-  username = random.choice(usernames)
-  usernames.remove(username)
-  return username
+	username = random.choice(usernames)
+	usernames.remove(username)
+	return username
 
 def run_seed(self, mode, number_of_users, assets_per_user, company_name):
-  clear_data()
-  if mode == MODE_CLEAR:
-    return
-  create_objects(number_of_users, assets_per_user, company_name)
+	clear_data()
+	if mode == MODE_CLEAR:
+		return
+	create_objects(number_of_users, assets_per_user, company_name)
 
 
 # Asset Types: ["Laptop", "Mobile", "Misc"]
