583b1a7f0abb24684586f4fb404f6660fab5cac1
IliasMariosG <57366310+IliasMariosG@users.noreply.github.com>
2020-10-16 17:52:27 +0300

    Ignore bad-continuation and invalid name error for 'urlpatterns' constant

--- AssetTracker/apps/dashboard/urls.py
+++ AssetTracker/apps/dashboard/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from .views import dashboardPageView
 
+# pylint: disable=bad-continuation
+# pylint: disable=invalid-name
 urlpatterns = [
     path('dashboard', dashboardPageView, name='home')
 ]
