956e6b2953182372d4f84309a10b8c61d927fc44
cmb84scd <cmb84scd@gmail.com>
2020-11-24 10:42:26 +0000

    Add test in progress

--- AssetTracker/apps/unittests/test_dashboard.py
+++ AssetTracker/apps/unittests/test_dashboard.py
@@ -3,6 +3,10 @@ from ..dashboard.views import countLaptops
 
 class Dashboard(SimpleTestCase):
 
-  def test_countLaptops(self):
+  def test_countLaptops_zero(self):
     assets = []
     self.assertEquals(countLaptops(assets), 0)
+
+  def test_countLaptops_one(self):
+    assets = ['Asset': {'device_type': 'Laptop'}]
+    self.assertEquals(countLaptops(assets), 1)
