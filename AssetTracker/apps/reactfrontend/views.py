f339d242ff5bf822434c7ebad5fb8a2f9e5d2ac7
Dawid Szpener <dawidszpener141@gmail.com>
2020-11-27 10:41:34 +0000

    finish reactfrontend unittests

--- AssetTracker/apps/reactfrontend/views.py
+++ AssetTracker/apps/reactfrontend/views.py
@@ -4,10 +4,10 @@ from django.contrib.auth.decorators import login_required
 @login_required(login_url='/login')
 
 def assets(request):
-    return render(request, 'reactfrontend/assets.html')
+  return render(request, 'reactfrontend/assets.html')
 
 def addAssets(request):
-    return render(request, 'reactfrontend/addAssets.html')
+  return render(request, 'reactfrontend/addAssets.html')
 
 def unauthorizedUsers(request):
   return render(request, 'reactfrontend/unauthorizedUsers.html')
--- AssetTracker/apps/unittests/test_reactfrontend.py
+++ AssetTracker/apps/unittests/test_reactfrontend.py
@@ -1,24 +1,27 @@
 from django.test import TestCase, Client
 from ..reactfrontend.views import *
+from django.contrib.auth.models import User
 
 class Reactfrontend(TestCase):
+  
   def setUp(self):
     self.client = Client()
+    user = User.objects.create(username='testuser')
+    user.set_password('12345')
+    user.save()
+    self.client.login(username='testuser', password='12345')
 
   def test_assets(self):
-    response = self.client.get('/assets')
+    response = self.client.get('/assets', follow=True)
     self.assertEqual(response.status_code, 200)
     self.assertTemplateUsed(response, 'reactfrontend/assets.html')
-    self.assertContains(response, 'Your Assets')
 
   def test_addAssets(self):
-    response = self.client.get('/assets/add')
+    response = self.client.get('/assets/add', follow=True)
     self.assertEqual(response.status_code, 200)
     self.assertTemplateUsed(response, 'reactfrontend/addAssets.html')
-    self.assertContains(response, 'Add an Asset')
 
   def test_unauthorizedUsers(self):
-    response = self.client.get('/usermanagement/unauthorized')
+    response = self.client.get('/usermanagement/unauthorized', follow=True)
     self.assertEqual(response.status_code, 200)
     self.assertTemplateUsed(response, 'reactfrontend/unauthorizedUsers.html')
-    self.assertContains(response, 'Unauthorized Users')
