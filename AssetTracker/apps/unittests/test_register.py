ae3ec348790311941e8db766d3dac4da0b971733
cmb84scd <cmb84scd@gmail.com>
2020-12-01 11:16:09 +0000

    Add test for register user page view

--- AssetTracker/apps/unittests/test_register.py
+++ AssetTracker/apps/unittests/test_register.py
@@ -20,3 +20,13 @@ class RegisterCompanyTest(TestCase):
     response = self.client.get('/registercompany', follow=True)
     self.assertEqual(response.status_code, 200)
     self.assertTemplateUsed(response, 'register/registercompany.html')
+
+class RegisterUserTest(TestCase):
+
+  def setUp(self):
+    self.client = Client()
+
+  def test_register_user_page_view(self):
+    response = self.client.get('/registeruser', follow=True)
+    self.assertEqual(response.status_code, 200)
+    self.assertTemplateUsed(response, 'register/registeruser.html')
