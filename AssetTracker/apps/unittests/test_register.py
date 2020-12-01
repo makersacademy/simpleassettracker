84d8075d2cd8824dfddd416b7b94ba92fa8dea7f
cmb84scd <cmb84scd@gmail.com>
2020-12-01 11:14:59 +0000

    Add test for register company page view

--- AssetTracker/apps/unittests/test_register.py
+++ AssetTracker/apps/unittests/test_register.py
@@ -10,3 +10,13 @@ class PreRegisterTest(TestCase):
     response = self.client.get('/preregister', follow=True)
     self.assertEqual(response.status_code, 200)
     self.assertTemplateUsed(response, 'register/preregister.html')
+
+class RegisterCompanyTest(TestCase):
+
+  def setUp(self):
+    self.client = Client()
+
+  def test_register_company_page_view(self):
+    response = self.client.get('/registercompany', follow=True)
+    self.assertEqual(response.status_code, 200)
+    self.assertTemplateUsed(response, 'register/registercompany.html')
