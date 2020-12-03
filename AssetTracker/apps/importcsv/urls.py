6db8de4b9456ad7441314cdfd527d7114b6ca56a
cmb84scd <cmb84scd@gmail.com>
2020-12-03 10:39:25 +0000

    Change method name in importcsv

--- AssetTracker/apps/importcsv/urls.py
+++ AssetTracker/apps/importcsv/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('import/', views.importView, name="import"),
+    path('import/', views.importcsv_view, name="importcsv"),
     url(r'^upload/csv/$', views.upload_csv, name='upload_csv'),
 ]
