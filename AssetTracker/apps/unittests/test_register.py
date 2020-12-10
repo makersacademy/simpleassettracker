505806dbdfd10a5fa06bea3afa9621fd71c787d9
cmb84scd <cmb84scd@gmail.com>
2020-12-10 11:14:22 +0000

    Add test for unregistered user api

--- AssetTracker/apps/unittests/test_register.py
+++ AssetTracker/apps/unittests/test_register.py
@@ -1,5 +1,8 @@
-from django.test import TestCase, Client
-from ..register.views import pre_register_view, register_company, register_user
+from django.test import TestCase, Client, RequestFactory
+from ..register.views import *
+from django.contrib.auth.models import User
+from ..companyusers.models import CompanyUser
+from ..companies.models import Company
 
 class PreRegisterTest(TestCase):
 
@@ -30,3 +33,18 @@ class RegisterUserTest(TestCase):
     response = self.client.get('/registeruser', follow=True)
     self.assertEqual(response.status_code, 200)
     self.assertTemplateUsed(response, 'register/registeruser.html')
+
+class UnauthorizedUserTest(TestCase):
+
+  def test_get_queryset_with_company(self):
+    user = User.objects.create(username='user1', password='12345', email='testuser@test.com', is_active=True)
+    user.save()
+    company = Company(name="Makers")
+    company.save()
+    CompanyUser.objects.create(user=user, company=company)
+    request = RequestFactory().get('unauthorizedusers/api/unauthorizedusers')
+    view = UnauthorizedUserList()
+    request.user = user
+    view.request = request
+    qs = view.get_queryset()
+    self.assertEqual(list(qs), list(UnauthorizedUser.objects.all()))
