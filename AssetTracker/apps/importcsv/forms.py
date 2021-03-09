9978293750ed0726c915a12847c442aac2fd6371
cmb84scd <cmb84scd@gmail.com>
2021-03-09 11:44:49 +0000

    Change add asset form to choose asset first, then show form

--- AssetTracker/apps/importcsv/forms.py
+++ AssetTracker/apps/importcsv/forms.py
@@ -2,6 +2,13 @@ from django import forms
 from AssetTracker.apps.assets.models import Asset
 
 class AssetForm(forms.ModelForm):
+  
+  serial_number = forms.CharField(required=False)
+  screen_size = forms.CharField(required=False)
+  hard_drive = forms.CharField(required=False)
+  ram = forms.CharField(required=False)
+  year = forms.CharField(required=False)
+
   class Meta:
     model = Asset
     fields = ("asset_tag", "device_type", "device_model", "asset_status", "serial_number", "asset_condition", "screen_size", "hard_drive", "ram", "year", "created_by", "company")
--- AssetTracker/apps/reactfrontend/src/components/addAssetForm/addAssetForm.js
+++ AssetTracker/apps/reactfrontend/src/components/addAssetForm/addAssetForm.js
@@ -8,21 +8,24 @@ class AddAssetForm extends Component {
       selectedAsset: '',
       asset: {
         assetTag: '',
-        deviceType: 'Laptop',
-        deviceModel: 'unassigned',
+        deviceType: '',
+        deviceModel: '',
         createdBy: '',
         assetStatus: 'Ready',
         assetCondition: 'Good',
-        serialNumber: '',
-        year: 'unassigned',
-        hardDrive: 'unassigned',
-        screenSize: 'unassigned',
-        ram: 'unassigned',
+        serialNumber: null,
+        year: null,
+        hardDrive: null,
+        screenSize: null,
+        ram: null,
+        imei: null,
+        storage: null,
+        colour: null,
       },
       messageType: '',
-      company: "",
+      company: '',
       showMessage: false,
-      companyusers: "",
+      companyusers: '',
     }
     this.submitHandler = this.submitHandler.bind(this)
     this.changeHandler = this.changeHandler.bind(this)
@@ -92,12 +95,15 @@ class AddAssetForm extends Component {
         "screen_size": this.state.asset.screenSize,
         "hard_drive": this.state.asset.hardDrive,
         "company": this.state.company,
+        "imei": this.state.imei,
+        "storage": this.state.storage,
+        "colour": this.state.colour,
       }),
     })
     .then(response => {
       if (response.ok) {
         this.setState({ showMessage: true, messageType: "successMessage" })
-        $('#id_add_asset')[0].reset(); 
+        $('#id_choose_asset')[0].reset(); 
         return response.json()
       } else {
         this.setState({ showMessage: true, messageType: "failMessage" })
@@ -124,8 +130,19 @@ class AddAssetForm extends Component {
     this.setState({showMessage: false})
   }
 
+  select_device_type() {
+    if (this.state.selectedAsset === 'laptop_form') {
+      return 'Laptop'
+    } else {
+      return 'Mobile'
+    }
+  }
+
   selectAssetHandler(event) { 
-    this.setState({selectedAsset: event.target.value})
+    let newState = this.state
+    newState.selectedAsset = event.target.value
+    newState.asset.deviceType = this.select_device_type()
+    this.setState(newState)
   }
 
   assetTagIsUnique() {
@@ -189,13 +206,9 @@ class AddAssetForm extends Component {
                 <input className="add_asset_input" inputtype='input' required type="text" onChange={(event) => this.changeHandler(event, 'assetTag')} name="assetTag" id="id_add_asset_tag"></input>
                 <label className="asset_add_title" htmlFor="id_add_serial_number">Serial Number:</label>
                 <input className="add_asset_input" inputtype='input' required type="text" onChange={(event) => this.changeHandler(event,'serialNumber')} name="serialNumber" id="id_add_serial_number"></input>
-                <label className="asset_add_title" htmlFor="id_add_asset_type" >Asset Type:</label>
-                <select defaultValue='Laptop' name="deviceType" id="id_add_asset_type" className="add_asset_input" onChange={(event) => this.changeHandler(event, 'deviceType')}>
-                  <option value="Laptop">Laptop</option>
-                </select>
                 <label className="asset_add_title" htmlFor="id_add_asset_model">Asset Model:</label>
-                <select defaultValue='unassigned' name="deviceModel" id="id_add_asset_model" className="add_asset_input" onChange={(event) => this.changeHandler(event, 'deviceModel')}>
-                  <option value="unassigned">Select model...</option>
+                <select name="deviceModel" id="id_add_asset_model" className="add_asset_input" onChange={(event) => this.changeHandler(event, 'deviceModel')}>
+                  <option>Select model...</option>
                   <option value="Air">Air</option>
                   <option value="MacBook">MacBook</option>
                   <option value="Pro">Pro</option>
@@ -222,8 +235,8 @@ class AddAssetForm extends Component {
                   <option value="Unavailable">Unavailable</option>
                 </select>
                 <label className="asset_add_title" htmlFor="id_add_year">Year:</label>
-                <select defaultValue='unassigned' name="year" id="id_add_year" className="add_asset_input" onChange={(event) => this.changeHandler(event, 'year')}>
-                  <option value="unassigned">Select year...</option>
+                <select name="year" id="id_add_year" className="add_asset_input" onChange={(event) => this.changeHandler(event, 'year')}>
+                  <option>Select year...</option>
                   <option value="2012">2012</option>
                   <option value="2013">2013</option>
                   <option value="2014">2014</option>
@@ -236,24 +249,24 @@ class AddAssetForm extends Component {
                   <option value="2021">2021</option>
                 </select>
                 <label className="asset_add_title" htmlFor="id_add_ram">Ram:</label>
-                <select defaultValue='unassigned' name="ram" id="id_add_ram" className="add_asset_input" onChange={(event) => this.changeHandler(event, 'ram')}>
-                  <option value="unassigned">Select size...</option>
+                <select name="ram" id="id_add_ram" className="add_asset_input" onChange={(event) => this.changeHandler(event, 'ram')}>
+                  <option>Select size...</option>
                   <option value="8GB">8GB</option>
                   <option value="16GB">16GB</option>
                   <option value="24GB">24GB</option>
                   <option value="32GB">32GB</option>
                 </select>
                 <label className="asset_add_title" htmlFor="id_add_hard_drive">Hard Drive:</label>
-                <select defaultValue='unassigned' name="hardDrive" id="id_add_hard_drive" className="add_asset_input" onChange={(event) => this.changeHandler(event, 'hardDrive')}>
-                  <option value="unassigned">Select size...</option>
+                <select name="hardDrive" id="id_add_hard_drive" className="add_asset_input" onChange={(event) => this.changeHandler(event, 'hardDrive')}>
+                  <option>Select size...</option>
                   <option value="128GB">128GB</option>
                   <option value="256GB">256GB</option>
                   <option value="512GB">512GB</option>
                   <option value="1TB">1TB</option>
                 </select>
                 <label className="asset_add_title" htmlFor="id_add_screen_size">Screen Size:</label>
-                <select defaultValue='unassigned' name="screenSize" id="id_add_screen_size" className="add_asset_input" onChange={(event) => this.changeHandler(event, 'screenSize')}>
-                  <option value="unassigned">Select size...</option>
+                <select name="screenSize" id="id_add_screen_size" className="add_asset_input" onChange={(event) => this.changeHandler(event, 'screenSize')}>
+                  <option>Select size...</option>
                   <option value="12 inches">12 inches</option>
                   <option value="13 inches">13 inches</option>
                   <option value="15 inches">15 inches</option>
@@ -262,8 +275,48 @@ class AddAssetForm extends Component {
                 <button className='btn btn-primary' id="id_add_asset_submit" style={{marginTop:"14px", clear:'both'}} type="submit" value="submit">Add Asset</button>
               </form>;
       case 'mobile_form':
-        return <form name="mobile_form" id="mobile_form">
-              Mobile form
+        return <form name="mobile_form" id="mobile_form" onSubmit={this.submitHandler}>
+                <label className="asset_add_title" htmlFor="id_add_asset_tag">Asset Tag:</label>
+                <input className="add_asset_input" inputtype='input' required type="text" onChange={(event) => this.changeHandler(event, 'assetTag')} name="assetTag" id="id_add_asset_tag"></input>
+                <label className="asset_add_title" htmlFor="id_add_imei">IMEI:</label>
+                <input className="add_asset_input" inputtype='input' required type="text" onChange={(event) => this.changeHandler(event,'imei')} name="imei" id="id_add_imei"></input>
+                <label className="asset_add_title" htmlFor="id_add_asset_model">Asset Model:</label>
+                <input className="add_asset_input" inputtype='input' required type="text" onChange={(event) => this.changeHandler(event,'deviceModel')} name="deviceModel" id="id_add_asset_model"></input>
+                <label className="asset_add_title" htmlFor="id_add_asset_condition" >Asset Condition:</label>
+                <select defaultValue='Good' name="assetCondition" id="id_add_asset_condition" className="add_asset_input" onChange={(event) => this.changeHandler(event, 'assetCondition')}>
+                  <option value="Good">Good</option>
+                  <option value="Bad">Bad</option>
+                  <option value="Okay">Okay</option>
+                  <option value="Broken">Broken</option>
+                  <option value="New">New</option>
+                </select>
+                <label className="asset_add_title" htmlFor="id_add_asset_status" >Asset Status:</label>
+                <select defaultValue='Ready' name="assetStatus" id="id_add_asset_status" className="add_asset_input" onChange={(event) => this.changeHandler(event, 'assetStatus')}>
+                  <option value="Ready">Ready</option>
+                  <option value="In Repair">In Repair</option>
+                  <option value="Locked at the office">Locked at the office</option>
+                  <option value="On Loan">On Loan</option>
+                  <option value="Needs Resetting">Needs Resetting</option>
+                  <option value="Extended Loan">Extended Loan</option>
+                  <option value="Unknown">Unknown</option>
+                  <option value="Lost">Lost</option>
+                  <option value="Stolen">Stolen</option>
+                  <option value="Unavailable">Unavailable</option>
+                </select>
+                <label className="asset_add_title" htmlFor="id_add_storage">Storage:</label>
+                <select name="storage" id="id_add_storage" required className="add_asset_input" onChange={(event) => this.changeHandler(event, 'storage')}>
+                  <option>Select storage...</option>
+                  <option value="64GB">64GB</option>
+                  <option value="128GB">128GB</option>
+                  <option value="256GB">256GB</option>
+                </select>
+                <label className="asset_add_title" htmlFor="id_add_colour">Colour:</label>
+                <select name="colour" id="id_add_colour" required className="add_asset_input" onChange={(event) => this.changeHandler(event, 'colour')}>
+                  <option>Select colour...</option>
+                  <option value="White">White</option>
+                  <option value="Space Grey">Space Grey</option>
+                </select>
+                <button className='btn btn-primary' id="id_add_asset_submit" style={{marginTop:"14px", clear:'both'}} type="submit" value="submit">Add Asset</button>
               </form>;
       default:
         return null;
@@ -297,8 +350,8 @@ class AddAssetForm extends Component {
         <div className="add_asset_container">
           <h1>Add an Asset</h1>
           <form id='id_choose_asset'>
-            <select id='id_select_asset_type' value={this.state.selectedAsset} onChange={this.selectAssetHandler}>
-              <option value=''>Select an asset to add...</option>
+            <select id='id_select_asset_type' onChange={this.selectAssetHandler}>
+              <option>Select an asset to add...</option>
               <option value='laptop_form'>Laptop</option>
               <option value='mobile_form'>Mobile</option>
             </select>
