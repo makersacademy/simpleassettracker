c778cfe86de20cf2ed69e17529e8bdcf77f98afe
cmb84scd <cmb84scd@gmail.com>
2021-04-06 15:22:38 +0100

    Save data to database

--- AssetTracker/apps/reactfrontend/src/components/importcsv/importcsv.js
+++ AssetTracker/apps/reactfrontend/src/components/importcsv/importcsv.js
@@ -6,19 +6,47 @@ class uploadCSV extends React.Component {
   constructor() {
     super();
     this.state = {
-      csvfile: undefined
+      csvfile: undefined,
+      company: null,
+      error_message: null
     };
     this.handleChange = this.handleChange.bind(this);
     this.saveData = this.saveData.bind(this);
     this.importCSV = this.importCSV.bind(this);
   }
 
+  componentDidMount() {
+    this.getCompanyID()
+  }
+
+  getCookie(name) {
+    let nameEQ = name + "=";
+    let ca = document.cookie.split(';');
+    for(let i=0;i < ca.length;i++) {
+      let c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  }
+
+  getCompanyID(){
+    fetch('/companyusers/api/companyusers/'+ window.django.user.user_id)
+    .then(response => {
+      if (response.status > 400) {
+        this.setState({error_message:"Something went wrong!"})
+      }
+    return response.json();
+    })
+    .then(data => {
+      this.setState({company:data.company});
+    });
+  }
+
   handleChange(event) {
-    console.log(event.target.files[0])
     this.setState({
       csvfile: event.target.files[0]
     });
-    console.log(this.state.csvfile);
   };
 
   importCSV() {
@@ -45,12 +73,34 @@ class uploadCSV extends React.Component {
   }
 
   saveData(result) {
-    let data = this.emptyStringCheck(result);
-    console.log(data);
+    let data = this.emptyStringCheck(result);    
+    let csrfToken = this.getCookie('csrftoken')
+
+    data.forEach(asset => {
+      fetch('/assets/api/asset/', {
+        method: 'POST',
+        headers: {
+          "X-CSRFToken": csrfToken,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          ...asset,
+          created_by:window.django.user.user_id,
+          company:this.state.company
+        }),
+      })
+      .then(response => {
+        if (response.ok) { 
+          return response.json()
+        } else {
+          throw new Error('Something went wrong ...');
+        }
+      })
+      .catch(error => (console.log(error)));
+    })
   }
 
   render() {
-    console.log(this.state.csvfile);
     return (
       <div className="App">
         <h2>Import CSV File!</h2>
@@ -66,6 +116,7 @@ class uploadCSV extends React.Component {
         />
         <p />
         <button onClick={this.importCSV}> Upload now!</button>
+        <p>{this.state.error_message}</p>
       </div>
     );
   }
