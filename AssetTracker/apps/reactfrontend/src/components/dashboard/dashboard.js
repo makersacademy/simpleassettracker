52d0ad54003df333394cea5472b01475d44ccfe8
Ben <ben.lynch1931@gmail.com>
2021-04-20 11:10:25 +0100

    create dashboard component base file

---
+++ AssetTracker/apps/reactfrontend/src/components/dashboard/dashboard.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import * as ReactBootStrap from 'react-bootstrap';
+import './dashboard.css';
+
+
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+  
+  componentDidMount() {
+    
+  }
+  
+  render() {
+    
+    return (
+      
+    )
+  }
+}
+
+export default Dashboard;
