5d22ac1f7f717581f7ad7815b24144eb55576919
Ben <ben.lynch1931@gmail.com>
2021-04-20 12:03:18 +0100

    add dashboard path and invoke to frontend urls

--- AssetTracker/apps/reactfrontend/urls.py
+++ AssetTracker/apps/reactfrontend/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('assets/', views.assets),
     path('assets/add', views.add_assets),
     path('usermanagement/unauthorized', views.unauthorized_users),
-    path('import/', views.importcsv)
+    path('import/', views.importcsv),
+    path('dashboard', views.dashboard)
 ]
